7491aa3eccc35743105218f54a55d1846cfc1901
- <your_email@whatever.com>
2022-07-19 19:24:55 +0300

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Ник К on 19.07.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+   
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLable: UILabel!
+    @IBOutlet weak var locationLable: UILabel!
+    @IBOutlet weak var tupeLable: UILabel!
+    
+
+}
